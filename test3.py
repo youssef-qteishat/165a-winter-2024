4f14771abcb6f0af31c7945eb8932af070d889d4
Luke Brenner <Luke Brenner>
2024-02-26 15:55:47 -0800

    Increased bufferpool size

--- test3.py
+++ test3.py
@@ -1,14 +1,38 @@
 from lstore.db import Database
 from lstore.query import Query
+import cProfile
+from random import choice, randrange
 
 db = Database()
 db.open('./ECS165')
 grades_table = db.create_table('Grades', 5, 0)
 query = Query(grades_table)
-query.insert(*[1, 2, 3, 4, 5])
-db.close()
-db.open('./ECS165')
-grades_table = db.get_table('Grades')
-query = Query(grades_table)
-print(query.select(1, 0, [1, 1, 1, 1, 1]))
+keys = []
+profiler = cProfile.Profile()
+
+profiler.enable()
+for i in range(0, 1000):
+    query.insert(906659671 + i, 93, 0, 0, 0)
+    keys.append(906659671 + i)
+profiler.disable()
+
+profiler.print_stats(sort='cumulative')
+
+
+# Measuring update Performance
+update_cols = [
+    [None, None, None, None, None],
+    [None, randrange(0, 100), None, None, None],
+    [None, None, randrange(0, 100), None, None],
+    [None, None, None, randrange(0, 100), None],
+    [None, None, None, None, randrange(0, 100)],
+]
+
+profiler.enable()
+for i in range(0, 1000):
+    query.update(choice(keys), *(choice(update_cols)))
+profiler.disable()
+
+profiler.print_stats(sort='cumulative')
+
 db.close()
